8eeabb5ef011ed31fbfd9b6df553dd6098747838
Solar Bear <97406365+solarbearcub@users.noreply.github.com>
2024-02-11 19:02:44 +0000

    Use formatters in all cases

--- secretlounge_ng/telegram.py
+++ secretlounge_ng/telegram.py
@@ -748,7 +748,7 @@ def relay_inner(ev, *, caption_text=None, signed=False, tripcode=False):
 	force_caption = None
 	if is_forward(ev):
 		pass # leave message alone
-	elif ev.content_type == "text" or ev.caption is not None or caption_text is not None:
+	elif:
 		fmt = FormattedMessageBuilder(caption_text, ev.caption, ev.text)
 		formatter_replace_links(ev, fmt)
 		formatter_network_links(fmt)
